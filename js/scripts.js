c4948d0705d98eed50c45ce029c9bdbfd98a255c
JakFranceschini <jakson.franceschini@gmail.com>
2022-10-31 20:59:50 -0300

    Versão 1.0 do portifolio.

---
+++ js/scripts.js
@@ -0,0 +1,109 @@
+let botaoVeja0 = document.getElementsByClassName('botaoVeja')[0];
+let botaoVeja1 = document.getElementsByClassName('botaoVeja')[1];
+let botaoLink0 = document.getElementsByClassName('botaoLink')[0];
+let botaoLink1 = document.getElementsByClassName('botaoLink')[1];
+let botaoLink2 = document.getElementsByClassName('botaoLink')[2];
+let botaoLink3 = document.getElementsByClassName('botaoLink')[3];
+let botaoContato0 = document.getElementsByClassName('botaoContato')[0];
+let botaoContato1 = document.getElementsByClassName('botaoContato')[1];
+let botaoContato2 = document.getElementsByClassName('botaoContato')[2];
+let botaoContato3 = document.getElementsByClassName('botaoContato')[3];
+let seta0 = document.getElementsByClassName('seta')[0];
+let seta1 = document.getElementsByClassName('seta')[1];
+let seta2 = document.getElementsByClassName('seta')[2];
+let seta3 = document.getElementsByClassName('seta')[3];
+let seta4 = document.getElementsByClassName('seta')[4];
+let seta5 = document.getElementsByClassName('seta')[5];
+let seta6 = document.getElementsByClassName('seta')[6];
+let seta7 = document.getElementsByClassName('seta')[7];
+let seta8 = document.getElementsByClassName('seta')[8];
+let seta9 = document.getElementsByClassName('seta')[9];
+
+botaoVeja0.addEventListener('mouseover', function() {
+    seta0.style.marginLeft = '10px';
+    seta0.style.transition = 'all 0.3s';
+});
+
+botaoVeja0.addEventListener('mouseout', function() {
+    seta0.style.marginLeft = '5px';
+});
+
+botaoVeja1.addEventListener('mouseover', function() {
+    seta1.style.marginLeft = '10px';
+    seta1.style.transition = 'all 0.3s';
+});
+
+botaoVeja1.addEventListener('mouseout', function() {
+    seta1.style.marginLeft = '5px';
+});
+
+botaoLink0.addEventListener('mouseover', function() {
+    seta2.style.marginLeft = '10px';
+    seta2.style.transition = 'all 0.3s';
+});
+
+botaoLink0.addEventListener('mouseout', function() {
+    seta2.style.marginLeft = '5px';
+});
+
+botaoLink1.addEventListener('mouseover', function() {
+    seta3.style.marginLeft = '10px';
+    seta3.style.transition = 'all 0.3s';
+});
+
+botaoLink1.addEventListener('mouseout', function() {
+    seta3.style.marginLeft = '5px';
+});
+
+botaoLink2.addEventListener('mouseover', function() {
+    seta4.style.marginLeft = '10px';
+    seta4.style.transition = 'all 0.3s';
+});
+
+botaoLink2.addEventListener('mouseout', function() {
+    seta4.style.marginLeft = '5px';
+});
+
+botaoLink3.addEventListener('mouseover', function() {
+    seta5.style.marginLeft = '10px';
+    seta5.style.transition = 'all 0.3s';
+});
+
+botaoLink3.addEventListener('mouseout', function() {
+    seta5.style.marginLeft = '5px';
+});
+
+botaoContato0.addEventListener('mouseover', function() {
+    seta6.style.marginLeft = '10px';
+    seta6.style.transition = 'all 0.3s';
+});
+
+botaoContato0.addEventListener('mouseout', function() {
+    seta6.style.marginLeft = '5px';
+});
+botaoContato1.addEventListener('mouseover', function() {
+    seta7.style.marginLeft = '10px';
+    seta7.style.transition = 'all 0.3s';
+});
+
+botaoContato1.addEventListener('mouseout', function() {
+    seta7.style.marginLeft = '5px';
+});
+
+botaoContato2.addEventListener('mouseover', function() {
+    seta8.style.marginLeft = '10px';
+    seta8.style.transition = 'all 0.3s';
+});
+
+botaoContato2.addEventListener('mouseout', function() {
+    seta8.style.marginLeft = '5px';
+});
+
+botaoContato3.addEventListener('mouseover', function() {
+    seta9.style.marginLeft = '10px';
+    seta9.style.transition = 'all 0.3s';
+});
+
+botaoContato3.addEventListener('mouseout', function() {
+    seta9.style.marginLeft = '5px';
+});
